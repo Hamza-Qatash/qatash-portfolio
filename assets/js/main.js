34832ae72a63bdf3cda5854016a0a10e1f0b8a68
Hamza <qatashh29@gmail.com>
2022-12-08 13:43:59 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+AOS.init();
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
+
+// Double Vertical Slider
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  console.log(sliderHeight);
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
+
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+
+//making the canvas
+c.height = window.innerHeight;
+c.width = 240;
+
+//characters - taken from the unicode charset
+var chars =
+  "REZEROQATASHZEROQATASHREZEROQATASHREZEROQATASHREZEROQATASHREZEROQATASHREZEROZEROQATASH";
+chars = chars.split("");
+
+var font_size = 10;
+var columns = c.width / font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for (var x = 0; x < columns; x++) drops[x] = 1;
+
+//drawing the characters
+function draw() {
+  //Black BG for the canvas
+  //translucent BG to show trail
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, c.width, c.height);
+
+  ctx.fillStyle = "#0F0"; //green text
+  ctx.font = font_size + "px arial";
+  //looping over drops
+  for (var i = 0; i < drops.length; i++) {
+    //a random chars character to print
+    var text = chars[Math.floor(Math.random() * chars.length)];
+    //x = i*font_size, y = value of drops[i]*font_size
+    ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+    //sending the drop back to the top randomly after it has crossed the screen
+    //adding a randomness to the reset to make the drops scattered on the Y axis
+    if (drops[i] * font_size > c.height && Math.random() > 0.975) drops[i] = 0;
+
+    //incrementing Y coordinate
+    drops[i]++;
+  }
+}
+
+setInterval(draw, 33);
